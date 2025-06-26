0f73bbacf5a392fe306a2c97894ef9ab3bdf7263
Samul Ovalle <sfovallerodriguez@gmail.com>
2025-06-25 20:22:16 -0500

    feat: UI effects and game improvements
    • Finished h1 text animation
    • Applied opacity transition from video to main
    • Moved game script to 'index_game_script.js'
    • Refactored game logic for better structure
    • Fixed bugs in ship movement (direction changes)

--- docs/files/js/pages/Index_script.js
+++ docs/files/js/pages/Index_script.js
@@ -3,11 +3,11 @@ import {game} from "../pages/index_game_script.js";
 const wait = (t) => new Promise(resolve => setTimeout(resolve, t));
 
 const start_logic = ()=>{
+    game();
     initial_animation.remove();
     document.querySelector("main").style.visibility = "visible";
     document.querySelector("main").style.opacity = "1";
     // document.querySelector("body").style.overflowY = "visible";
-    game();
 
     async function add_text(element, texts){
         const target = document.querySelector(`#${element}`);
@@ -33,147 +33,11 @@ const start_logic = ()=>{
     
     const cursor = document.querySelector(".cursor");
     setInterval(() => {cursor.style.visibility = (cursor.style.visibility === "hidden") ? "visible" : "hidden";}, 800);
-    
+
     setTimeout(() => {
-        
-        let texts = ["Samuel Ovalle is", "full-stack developer"];
+        let texts = ["Samuel Ovalle is", "Full-stack developer"];
         add_text("my_name", texts)
     }, 3000);
-}
-
-async function game_canvas (){
-    const canvas = document.getElementById("back_game");
-    const ctx = canvas.getContext("2d");
-
-    const window_height = window.innerHeight;
-    const window_width = window.innerWidth;
-
-    const cell_size = window_width/25;
-    const panel_cell = cell_size/2;
-
-    canvas.height = window_height;
-    canvas.width = window_width;
-
-    class ship {
-        constructor(ship, height, width, x, y, direction, status) {
-            this.ship = ship;
-            this.height = height;
-            this.width = width;
-            this.x = x;
-            this.y = y;
-            this.status = status;
-            this.direction = direction;
-        }
-        move(dt){
-            let dy = ["top", "bottom"]
-            let dx = ["left", "right"]
-
-            if (this.y === 1) dy = dy = ["bottom"]
-            if (this.y === Math.floor(window_height/panel_cell) - 1) dy = ["top"]
-
-            if (this.x === 1) dx = ["right"]        
-            if (this.x === Math.floor(window_width/panel_cell) - 1) dx = ["left"]
-            
-            let directions = dy.includes(dt) ? [dt, ...dx]
-            : dx.includes(dt) ? [dt, ...dy]
-            : [...dy, ...dx];
-            
-            let option_to_turn = Math.floor(Math.random() * directions.length) + 1;
-            
-            switch (directions[option_to_turn-1]) {
-                case "top":
-                    this.y = this.y - 1;
-                    this.direction = "top"
-                    this.ship.style.transform = "rotate(180deg)"
-                    this.ship.style.top = `${(panel_cell*this.y)-(this.height/2)}px`
-                    
-                    break;
-                case "left":
-                    this.x = this.x - 1;
-                    this.direction = "left"
-                    this.ship.style.transform = "rotate(90deg)"
-                    this.ship.style.left = `${(panel_cell*this.x)-(this.width/2)}px`
-
-                    break;
-                case "right":
-                    this.x = this.x + 1;
-                    this.direction = "right"
-                    this.ship.style.transform = "rotate(-90deg)"
-                    this.ship.style.left = `${(panel_cell*this.x)-(this.width/2)}px`
-                    
-                    break;
-                case "bottom":
-                    this.y = this.y + 1;
-                    this.direction = "bottom"
-                    this.ship.style.transform = "rotate(0deg)"
-                    this.ship.style.top = `${(panel_cell*this.y)-(this.height/2)}px`
-                    break;
-            }
-        }
-    }
-
-    const update_frame = ()=>{
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-        ctx.strokeStyle = "#00e2e1";
-        ctx.lineWidth = .6;
-        for (let x = 0; x <= canvas.width; x += cell_size) {
-            ctx.beginPath();
-            ctx.moveTo(x, 0);
-            ctx.lineTo(x, canvas.height);
-            ctx.stroke();
-        }
-        for (let y = 0; y <= canvas.height; y += cell_size) {
-            ctx.beginPath();
-            ctx.moveTo(0, y);
-            ctx.lineTo(canvas.width, y);
-            ctx.stroke();
-        }
-
-
-        // ship_1.move(ship_1.direction)
-        // ship_2.move(ship_2.direction)
-        // ship_3.move(ship_3.direction)
-        // ship_4.move(ship_4.direction)
-
-
-        let height
-        let width
-        // x, y, width, height
-        if (ship_1.direction == "top" || ship_1.direction == "bottom") {height = ship_1.height; width = ship_1.width}
-        else if (ship_1.direction == "left" || ship_1.direction == "right") {height = ship_1.width; width = ship_1.height}
-        ctx.fillStyle = "#00f0ff";
-        ctx.fillRect((panel_cell*ship_1.x)-(ship_1.height/2), (panel_cell*ship_1.y)-(ship_1.height/2), width, height); 
-
-        if (ship_2.direction == "top" || ship_2.direction == "bottom") {height = ship_2.height; width = ship_2.width}
-        else if (ship_2.direction == "left" || ship_2.direction == "right") {height = ship_2.width; width = ship_2.height}
-        ctx.fillStyle = "#ff0000";
-        ctx.fillRect((panel_cell*ship_2.x)-(ship_2.height/2), (panel_cell*ship_2.y)-(ship_2.height/2), width, height);
-
-        if (ship_3.direction == "top" || ship_3.direction == "bottom") {height = ship_3.height; width = ship_3.width}
-        else if (ship_3.direction == "left" || ship_3.direction == "right") {height = ship_3.width; width = ship_3.height}
-        ctx.fillStyle = "#6600ff";
-        ctx.fillRect((panel_cell*ship_3.x)-(ship_3.height/2), (panel_cell*ship_3.y)-(ship_3.height/2), width, height);
-
-        if (ship_4.direction == "top" || ship_4.direction == "bottom") {height = ship_4.height; width = ship_4.width}
-        else if (ship_4.direction == "left" || ship_4.direction == "right") {height = ship_4.width; width = ship_4.height}
-        ctx.fillStyle = "#ffff00";
-        ctx.fillRect((panel_cell*ship_4.x)-(ship_4.height/2), (panel_cell*ship_4.y)-(ship_4.height/2), width, height);
-    }
-
-    const all_ships = document.querySelectorAll(".ship")
-
-    let ship_1 = new ship(all_ships[0], all_ships[0].height, all_ships[0].width, 3, 3, "right", true);
-    let ship_2 = new ship(all_ships[1], all_ships[1].height, all_ships[1].width, 47, 3, "left", true);
-    let ship_3 = new ship(all_ships[2], all_ships[2].height, all_ships[2].width, 3, 22, "right", true);
-    let ship_4 = new ship(all_ships[3], all_ships[3].height, all_ships[3].width, 47, 22, "left", true);
-    
-    let i = true
-    update_frame()
-    while(i == true){
-        update_frame()
-        await wait(100)
-    }
 }
 
 const initial_animation = document.getElementById("initial_animation");
--- docs/files/js/pages/index_game_script.js
+++ docs/files/js/pages/index_game_script.js
@@ -1,6 +1,4 @@
 export const game = () => {
-    const wait = (t) => new Promise(resolve => setTimeout(resolve, t));
-
     async function update_frame() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
 
@@ -19,15 +17,10 @@ export const game = () => {
             ctx.stroke();
         }
 
-        ctx.fillStyle = "#00f0ff";
-        ctx.fillRect((panel_cell*ship_1.x)-(ship_1.height/2), (panel_cell*ship_1.y)-(ship_1.height/2), 10, 10); 
-        ctx.fillStyle = "#ff0000";
-        ctx.fillRect((panel_cell*ship_2.x)-(ship_2.height/2), (panel_cell*ship_2.y)-(ship_2.height/2), 10, 10);
-        ctx.fillStyle = "#6600ff";
-        ctx.fillRect((panel_cell*ship_3.x)-(ship_3.height/2), (panel_cell*ship_3.y)-(ship_3.height/2), 10, 10);
-        ctx.fillStyle = "#ffff00";
-        ctx.fillRect((panel_cell*ship_4.x)-(ship_4.height/2), (panel_cell*ship_4.y)-(ship_4.height/2), 10, 10);
-
+        ship_1.move(ship_1.direction);
+        ship_2.move(ship_2.direction);
+        ship_3.move(ship_3.direction);
+        ship_4.move(ship_4.direction);
     }
 
     const canvas = document.getElementById("back_game");
@@ -38,32 +31,105 @@ export const game = () => {
 
     const cell_size = window_width/25;
     const panel_cell = cell_size/2;
+    // x panel_cell = 50
+    // y panel_cell = 24
+
 
     canvas.height = window_height;
     canvas.width = window_width;
 
     class ship {
-        constructor(ship, height, width, x, y, direction, status) {
+        constructor(ship, height, width, x, y, direction, movement, angle, status, color) {
             this.ship = ship;
             this.height = height;
             this.width = width;
             this.x = x;
             this.y = y;
-            this.status = status;
             this.direction = direction;
+            this.movement = movement;
+            this.angle = angle;
+            this.status = status;
+
+            this.color = color;
         }
-        
+        move(dt){
+            let directions = [0, 1, 2, 3];
+
+            if (this.movement !== 0) {
+                this.movement = this.movement-1
+                switch (this.direction) {
+                    case 0:
+                        if (this.y < 2) this.movement = 0;
+                        else this.y = this.y - 1;
+                        this.ship.style.top = `${(panel_cell*this.y)-(this.height/2)}px`
+                        
+                        break;
+                    case 1:
+                        if (this.x > Math.floor(window_width/panel_cell)-2) this.movement = 0;
+                        else this.x = this.x + 1;
+                        this.ship.style.left = `${(panel_cell*this.x)-(this.width/2)}px`
+                        
+                        break;
+                    case 2:
+                        if (this.y > Math.floor(window_height/panel_cell)-1) this.movement = 0;
+                        else this.y = this.y + 1;
+                        this.ship.style.top = `${(panel_cell*this.y)-(this.height/2)}px`
+                        
+                        break;
+                    case 3:
+                        if (this.x < 2) this.movement = 0;
+                        else this.x = this.x - 1;
+                        this.ship.style.left = `${(panel_cell*this.x)-(this.width/2)}px`
+
+                        break;
+                }
+            }
+            if (this.movement === 0) {
+                if (dt<2) directions.splice(dt+2, 1);
+                else directions.splice(dt-2, 1);
+
+                if (this.y < 2) directions.splice(directions.indexOf(0), 1);
+                if (this.x > (window_width/panel_cell) - 2) directions.splice(directions.indexOf(1), 1);
+                if (this.y > (window_height/panel_cell) - 3) directions.splice(directions.indexOf(2), 1);
+                if (this.x < 2) directions.splice(directions.indexOf(4), 1);
+
+                this.movement = Math.floor(Math.random() * 4) + 2;
+                this.direction = directions[Math.floor(Math.random() * directions.length)];
+                let last_direction = dt;
+                let actual_direction = this.direction;
+                let deference = actual_direction-last_direction;
+                
+                if (deference === -1 || deference === 3) this.angle = this.angle - 90;
+                if (deference === 1 || deference === -3) this.angle = this.angle + 90;
+                this.ship.style.transform = `rotate(${this.angle}deg)`
+            }
+
+            let h 
+            // x y
+            let start_node = [3,3];
+            let end_node = [30,15];
+            let current_node = [3,3];
+            
+            let open_list = [
+                [current_node[0]+1, current_node[1]],
+                [current_node[0]-1, current_node[1]],
+                [current_node[0], current_node[1]+1],
+                [current_node[0], current_node[1]-1],
+            ];
+            let closed_list = [];
+            
+
+        }
     }
 
     const all_ships = document.querySelectorAll(".ship")
 
-    let ship_1 = new ship(all_ships[0], all_ships[0].height, all_ships[0].width, 3, 3, "right", true);
-    let ship_2 = new ship(all_ships[1], all_ships[1].height, all_ships[1].width, 47, 3, "left", true);
-    let ship_3 = new ship(all_ships[2], all_ships[2].height, all_ships[2].width, 3, 22, "right", true);
-    let ship_4 = new ship(all_ships[3], all_ships[3].height, all_ships[3].width, 47, 22, "left", true);
+    let ship_1 = new ship(all_ships[0], all_ships[0].height, all_ships[0].width, 3, 3, 1, 0, -90, true, "#00f0ff");
+    let ship_2 = new ship(all_ships[1], all_ships[1].height, all_ships[1].width, 47, 3, 3, 0, 90, true, "#ff0000");
+    let ship_3 = new ship(all_ships[2], all_ships[2].height, all_ships[2].width, 3, 22, 1, 0, -90, true, "#6600ff");
+    let ship_4 = new ship(all_ships[3], all_ships[3].height, all_ships[3].width, 47, 22, 3, 0, 90, true, "#ffff00");
 
-    setInterval(() => {
-        update_frame()
-    }, 100);
+    update_frame()
+    setInterval(() => {update_frame()}, 100);
 }
 
