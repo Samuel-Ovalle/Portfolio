abff6b3775a8e8ba50a29700d0a92ebea6d13184
Samul Ovalle <sfovallerodriguez@gmail.com>
2025-08-04 19:50:30 -0500

    feat: add images to carousel cards and create project icons, fix text formatting bug

--- docs/assets/task.md
+++ docs/assets/task.md
@@ -1,15 +1,11 @@
 * current task
-- add images on cards of carousel
 
 * task to finished
-- connect another projects whit my portfolio 
+- connect another projects whit my portfolio
 - make responsive the page
 - fix 3d model location error in the repository
 
 * task complete
-- add animation in card when carousel move
-- adjust 3d model in web page
-    - illumination
-    - position
-- add footer for contact and more info
-- add button to download my cv
+- add images on cards of carousel
+    - create projects icons
+- fix text bug on cards
--- docs/files/js/pages/index/index_3d_model.js
+++ docs/files/js/pages/index/index_3d_model.js
@@ -6,13 +6,11 @@ import { RenderPass }        from 'three/addons/postprocessing/RenderPass.js';
 import { UnrealBloomPass }   from 'three/addons/postprocessing/UnrealBloomPass.js';
 import { RectAreaLightUniformsLib } from 'three/addons/lights/RectAreaLightUniformsLib.js';
 
-function add_3d_model(container) {
+function add_3d_model(container, imgs) {
     /* ── 1. SCENE, CAMERA, RENDERER ────────────────────────────────────── */
     const scene  = new THREE.Scene();
     
-    const camera = new THREE.PerspectiveCamera(
-        75, window.innerWidth / window.innerHeight, 0.1, 1000
-    );
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     camera.position.set(-.01, .3, 2.1);          // camera position
     
     const renderer = new THREE.WebGLRenderer({ antialias: true });
@@ -21,10 +19,13 @@ function add_3d_model(container) {
     /*  Post-processing needs correct color management  */
     renderer.outputColorSpace = THREE.SRGBColorSpace;
     renderer.toneMapping      = THREE.ACESFilmicToneMapping;
+
+    let img_index = 0;
     
     document.getElementById(container).insertAdjacentHTML("afterbegin", 
         `
-        <div class="card"></div>
+        <div class="card">
+        </div>
         <div class="row L">
             <svg class="row_svg" id="row_l" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 82.6 56.86">
                 <polyline points="77.1 5.5 59.2 26.89 41.3 48.29 23.4 26.89 5.5 5.5" fill="none" stroke="#3fb1e6" stroke-linecap="round" stroke-miterlimit="10" stroke-width="11"/>
@@ -97,9 +98,52 @@ function add_3d_model(container) {
     }
     animate();
     
-    document.querySelector(`#${container} .L`).addEventListener("click", ()=>{end_angle += 45;})
-    document.querySelector(`#${container} .R`).addEventListener("click", ()=>{end_angle -= 45;})
+    document.querySelector(`#${container} .L`).addEventListener("click", ()=>{
+        end_angle += 45;
+        img_index = (img_index !== 0) ? img_index-1 : img_index = (imgs.length-1);
+
+        setTimeout(() => {
+            if (container === "technologies") {
+                document.querySelector(`#${container} .card`).textContent = imgs[img_index][0].replace(/_/g, " ");
+                document.querySelector(`#${container} .card`).insertAdjacentHTML("beforeend", `<img src="assets/img/icons/${imgs[img_index][0]}.png" alt=""> <p>${imgs[img_index][1]} years of experience</p>`)
+            }else if (container === "projects"){
+                document.querySelector(`#${container} .card`).textContent = imgs[img_index].replace(/_/g, " ");
+                document.querySelector(`#${container} .card`).insertAdjacentHTML("beforeend", `<img src="assets/img/icons/${imgs[img_index]}.png" alt=""> <a>Explore</a>`)
+            }
+        }, 500);
+    })
+    document.querySelector(`#${container} .R`).addEventListener("click", ()=>{
+        end_angle -= 45;
+        img_index = (img_index !== (imgs.length-1)) ? img_index+1 : img_index = 0;
+
+        setTimeout(() => {
+            if (container === "technologies") {
+                document.querySelector(`#${container} .card`).textContent = imgs[img_index][0].replace(/_/g, " ");
+                document.querySelector(`#${container} .card`).insertAdjacentHTML("beforeend", `<img src="assets/img/icons/${imgs[img_index][0]}.png" alt=""> <p>${imgs[img_index][1]} years of experience</p>`)
+            }else if (container === "projects"){
+                document.querySelector(`#${container} .card`).textContent = imgs[img_index].replace(/_/g, " ");
+                document.querySelector(`#${container} .card`).insertAdjacentHTML("beforeend", `<img src="assets/img/icons/${imgs[img_index]}.png" alt=""> <a>Explore</a>`)
+            }
+        }, 500);
+    })
 }
 
-add_3d_model("technologies")
-add_3d_model("projects")
+let technologies = [["HTML5", 4], ["CSS3", 4], ["JavaScript", 4], ["PHP", 3], ["React", 1], ["Python", 1], ["C++ (CPlusPlus)", 1], ["MySQL", 2], ["SQLite", 2], ["Git", 2], ["Github", 2], ["Blender", 2], ["Adobe_Illustrator", 5], ["Adobe_Photoshop", 1], ["Adobe_Premiere_Pro", 2]]
+add_3d_model("technologies", technologies)
+document.querySelector("#technologies .card").insertAdjacentHTML("afterbegin",
+    `
+        ${technologies[0][0].replace(/_/g, " ")}
+        <img src="assets/img/icons/${technologies[0][0]}.png" alt="">
+        <p>${technologies[0][1]} years of experience</p>
+    `
+)
+
+let projects = ["Movi-Grip", "Ships_Game", "3D_model", "Web_Designs", "Automatic_Whatsapp", "Calculators"]
+add_3d_model("projects", projects)
+document.querySelector("#projects .card").insertAdjacentHTML("afterbegin",
+    `
+        ${projects[0].replace(/_/g, " ")}
+        <img src="assets/img/icons/${projects[0]}.png" alt="">
+        <a href="">Explore</a>
+    `
+)
